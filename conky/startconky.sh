6b7a1a65b2e138db9610a1158c090cae601e7f3f
Roberto <rpungartnik@gmail.com>
2019-08-23 21:38:59 -0300

    Update Initialization Script

--- conky/startconky.sh
+++ conky/startconky.sh
@@ -2,4 +2,7 @@
 sleep 5
 conky -p 5 -c ~/.conky/conky_system &
 conky -p 5 -c ~/.conky/conky_weather &
-conky -p 5 -c ~/.conky/conky_market 
+conky -p 5 -c ~/.conky/conky_market &
+conky -p 5 -c ~/.conky/conky_earthquake &
+conky -p 5 -c ~/.conky/conky_TT_Brasil &
+conky -p 5 -c ~/.conky/conky_TT_Mundo
